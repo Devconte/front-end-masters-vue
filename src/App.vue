a652fdd35ce24ba2e0af289b2a668e38aaafe756
Devconte <romain.lcnte@gmail.com>
2023-12-19 10:29:13 +0100

    exercice ok

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const charactersList = [
+  { name: "Naruto" },
+  { name: "Sasuke" },
+  { name: "Sakura" },
+  { name: "Kiba" },
+  { name: "Kakashi" }
+];
 </script>
 
 <template>
-  
+  <h1> Naruto </h1>
+  <p v-if="charactersList.length === 0"> There are no characters</p>
+  <ul v-else>Characters list
+    <li v-for="character in charactersList" :key="character.name">{{ character.name }}</li>
+  </ul>
 </template>
-
-
